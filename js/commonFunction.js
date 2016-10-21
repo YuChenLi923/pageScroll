258fd3464b2519db4b735457ec7357e957d986fe
liyc1996 <swulll123@163.com>
2016-10-21 13:18:27 +0800

    更新至0.3

--- js/commonFunction.js
+++ js/commonFunction.js
@@ -1,7 +1,25 @@
+//处理样式表
+function handleStyleTable(dom){
+	var classes=dom.className,
+		ary=classes.split(' '),
+		ret=new Object();
+		ret.remove=function(className){
+				for(var i=0,len=ary.length;i<len;i++){
+					if(ary[i]===className){
+						ary.splice(i,1);
+					}
+				}
+				dom.className=ary.join(' ');
+		};
+		ret.add=function(className){
+				dom.className+=' '+className;
+		};
+	return ret;	
+}
+
 //向某个对象添加事件
 function addEvent(Obj,e,func) {
 	if(Obj.addEventListener){
-		console.log(e);
 		Obj.addEventListener(e,func);
 	}
 	else if(Obj.attachEvent){
@@ -17,29 +35,20 @@ function getPageSizeInf(){
 		scrollTop = window.pageYOffset|| doc.documentElement.scrollTop || doc.body.scrollTop,
 		viewHeight =Math.min(doc.documentElement.scrollHeight,doc.clientHeight),
 		docHeight=Math.max(doc.documentElement.scrollHeight,doc.documentElement.clientHeight),
-		scrollBottom=docHeight-viewHeight-scrollTop;
+		scrollBottom=docHeight-viewHeight-scrollTop,
+		viewWidth =Math.min(doc.documentElement.scrollWidth,doc.clientWidth),
+		docWidth=Math.max(doc.documentElement.scrollWidth,doc.documentElement.clientWidth);
 	return {
 		scrollTop:scrollTop,
 		viewHeight:viewHeight,
 		docHeight:docHeight,
-		scrollBottom:scrollBottom
+		scrollBottom:scrollBottom,
+		viewWidth:viewWidth,
+		docWidth:docWidth
 	};
 }
 
-//创建XMLHttpRequest对象，简称xhr对象
-function creatXHR(){
-	if(typeof XMLHttpRequest == 'undefined')
-		XMLHttpRequest=function(){
-			try{return new ActiveXObject("Msxml2.XMLHTTP.6.0");}
-			catch(e){}
-			try{return new ActiveXObject("Msxml2.XMLHTTP.3.0");}
-			catch(e){}
-			try{return new ActiveXObject("Msxml2.XMLHTTP");}
-			catch(e){}
-			return false;
-		}
-		return new XMLHttpRequest();
-}
+
 //最大程度优化元素获取
 function getElement(obj,select,dynamic){
 	var  doc=document,
@@ -84,4 +93,163 @@ function getElement(obj,select,dynamic){
 	}
 	return elem;
 }
+//创建二维空数组
+function twoDimArray(len){
+	var result=[];
+	for(var i=0;i<len;i++){
+		result.push([]);
+	}
+	return result;
+}
+//贝塞尔动画时间曲线函数，使用的时候调用其中的solve(x,epsilon)方法，参数x为当前动画执行的时间,参数eplsilon为精确度
+function UnitBezier(p1x,p1y,p2x,p2y) {
+		this.cx = 3.0 * p1x;
+		this.bx = 3.0 * (p2x - p1x) - this.cx;
+		this.ax = 1.0 - this.cx -this.bx;
+		 
+		this.cy = 3.0 * p1y;
+		this.by = 3.0 * (p2y - p1y) - this.cy;
+		this.ay = 1.0 - this.cy - this.by;//之后再研究
+}
+UnitBezier.prototype = {
+	epsilon : 1e-2,     // 默认精度
+	sampleCurveX : function(t) {//贝赛尔曲线t时刻的坐标点的X坐标
+	   	return ((this.ax * t + this.bx) * t + this.cx) * t;
+	},
+	sampleCurveY : function(t) {
+	   	return ((this.ay * t + this.by) * t + this.cy) * t;
+	},
+	sampleCurveDerivativeX : function(t) {//贝赛尔曲线t时刻的坐标点的Y坐标
+	    return (3.0 * this.ax * t + 2.0 * this.bx) * t + this.cx;
+	},
+	solveCurveX : function(x, epsilon) {
+			var t0,
+				t1,
+				t2,
+				x2,
+				d2,
+				i;
+
+			for (t2 = x, i = 0; i < 8; i++) {
+			    x2 = this.sampleCurveX(t2) - x;
+			    if (Math.abs (x2) < epsilon)
+			        return t2;
+			    d2 = this.sampleCurveDerivativeX(t2);
+			    if (Math.abs(d2) < epsilon)
+			        break;
+			    t2 = t2 - x2 / d2;
+			}
+
+			// No solution found - use bi-section
+			t0 = 0.0;
+			t1 = 1.0;
+			t2 = x;
+
+			if (t2 < t0) return t0;
+			if (t2 > t1) return t1;
+
+			while (t0 < t1) {
+				x2 = this.sampleCurveX(t2);
+				if (Math.abs(x2 - x) < epsilon)
+					return t2;
+				if (x > x2) t0 = t2;
+				else t1 = t2;
+
+				t2 = (t1 - t0) * .5 + t0;
+			}
+
+			// Give up
+			return t2;
+	},
+	solve : function(x, epsilon) {
+	   	return this.sampleCurveY( this.solveCurveX(x, epsilon) );
+	}
+}
+
+
+//动画函数 0.1
+var speedPattern={
+	ease:function(curTime,startValue,endValue,duration){
+		var Bezier=new UnitBezier(0.25,0.1,0.25,1);
+		return  Bezier.solve(curTime/duration,UnitBezier.prototype.epsilon)*(endValue-startValue)+startValue;
+	},
+	linear:function(curTime,startValue,endValue,duration){
+		var Bezier=new UnitBezier(0,0,1,1);
+		return  Bezier.solve(curTime/duration,UnitBezier.prototype.epsilon)*(endValue-startValue)+startValue;
+	},
+	easeIn:function(curTime,startValue,endValue,duration){
+		var Bezier=new UnitBezier(0.42,0,1,1);
+		return  Bezier.solve(curTime/duration,UnitBezier.prototype.epsilon)*(endValue-startValue)+startValue;
+	},
+	easeOut:function(curTime,startValue,endValue,duration){
+		var Bezier=new UnitBezier(0, 0, 0.58, 1);
+		return  Bezier.solve(curTime/duration,UnitBezier.prototype.epsilon)*(endValue-startValue)+startValue;
+	},
+	easeInOut:function(curTime,startValue,endValue,duration){
+		var Bezier=new UnitBezier(0.42, 0, 0.58, 1);
+		return  Bezier.solve(curTime/duration,UnitBezier.prototype.epsilon)*(endValue-startValue)+startValue;
+	}
+}
+function animate(dom){
+	this.dom=dom;
+	this.startTime=0;
+	this.startValue=[];
+	this.endValue=[];
+	this.propertyName=[];
+	this.unit=[];
+	this.easing=null;
+	this.duration=null;
+	this.func=null;
+}
+animate.prototype.start=function(rules,duration,easing){
+	this.startTime=+new Date;
+	this.duration=duration;
+	this.easing=speedPattern[easing];
+	var self=this;
+	for(var i=0,len=rules.length;i<len;i++){
+		(function(t){
+			var rule=rules[t].split(':');
+			self.propertyName[t]=rule[0];
+			if(self.unit[t]=rule[1].match(/[a-zA-Z]{1,}/)){
+				self.unit[t]=rule[1].match(/[a-zA-Z]{1,}/)[0];
+			}
+			else{
+				self.unit[t]='';
+			}
+			self.endValue[t]=parseInt(rule[1]);
+			self.startValue[t]=parseInt(self.dom.style[self.propertyName[t]])||0;
+		})(i);
+	}
+	var go=function(){
+		var t=+new Date;
+		if(t>=self.startTime+self.duration){
+			for(var i=0,len=rules.length;i<len;i++){
+				self.dom.style[self.propertyName[i]]=self.endValue[i]+self.unit[i];
+			}
+			if(self.func){
+				self.func();
+			}
+			return false;
+		}
+		else{
+			var cur=[];
+			for(var i=0,len=rules.length;i<len;i++){
+				(function(i){
+					cur[i]=self.easing(t-self.startTime,self.startValue[i],self.endValue[i],self.duration);
+					self.update(cur[i],self.propertyName[i],self.unit[i]);
+				})(i);
+			}	
+			setTimeout(function(){
+				go();
+			},13);
+		}
+	};
+	go();
+}
+animate.prototype.update=function(cur,propertyName,unit){
+	this.dom.style[propertyName]=cur+unit;
+}
+animate.prototype.callback=function(func){
+	this.func=func;
+};
 
--- js/pageScroll.js
+++ js/pageScroll.js
@@ -5,44 +5,54 @@
 		direction=data.direction,
 		time=data.time,
 		style=data.style,
-		index=1,
+		nav=data.nav,
+		index=data.start,
 		animation=true,
-		pages=doc.getElementById('warp').firstElementChild||doc.getElementById('warp').childNodes[1],
-		height=getPageSizeInf().docHeight;
+		ul=null,
+		loop=data.loop,
+		control=data.control,
+		start=data.start,
+		time=data.time,
+		warp=doc.getElementById('warp'),
+		pages=warp.firstElementChild||doc.getElementById('warp').childNodes[1],
+		height=getPageSizeInf().docHeight,
+		width=getPageSizeInf().docWidth,
 		speed=height/(100*time),
-		page=getElement(pages,'.page',false);
-		for(var i=0,len=page.length;i<len;i++){
-			page[i].style.height=height+'px';
-		}
-		// console.log(pages);
-		if(window.navigator.userAgent.toLowerCase().indexOf('firefox')!=-1){
-			addEvent(doc,'DOMMouseScroll',function(e){
-				scrollEvent(e);
-			});
-		}
-		else{
-			addEvent(doc,'mousewheel',function(e){
-				scrollEvent(e);
-
-			});
-		}
-		addEvent(window,'resize',function(e){
-			return false;
-		})
-	function scrollEvent(e){
-		var e=e || window.event;
-		//向下滚动
-		if(e.wheelDelta<0||e.detail>0){
-			if(animation&&index<num){
-				scrollAnimation(1);
+		page=getElement(pages,'.page',false),
+		cssText=twoDimArray(num);
+	//页面初始化
+	function init(start){
+		curNav(num,start);
+		var curPClass=handleStyleTable(page[start-1]);
+		curPClass.add('cur');
+		curPClass=null;
+	}
+	//处理页面滚动的起始位置
+	function scroll(flag){
+		var from;
+		if(animation&&(loop||(index<=num&&index>=1))){
+			if(loop||(index+flag<=num&&index+flag>=1)){
+				from=index;
+				if(index+flag>num){
+					index=1;
+				}
+				else if(index+flag<1){
+					index=num;
+				}
+				else{
+					index=index+flag;
+				}
+				pageScrollAnimation(from,index);
 			}
 		}
-		//向上滚动
-		if(e.wheelDelta>0||e.detail<0){
-			if(animation&&index>1){
-				scrollAnimation(-1);
-			}
-		}
+	}
+	//鼠标滚轮事件处理程序
+	function wheelEvent(e){
+		var e=e || window.event,
+			flag;
+		//1-向下滚动,-1-向上滚动
+			flag=e.wheelDelta<0||e.detail>0?1:-1;
+			scroll(flag);
 		if (e.preventDefault) {
 			e.preventDefault();
 		} 
@@ -50,37 +60,143 @@
 			e.returnValue = false;
 		}
 	}
-	function scrollAnimation(flag){
-		animation=false;
-		var cur=Math.abs(pages.offsetTop),
-			pre=cur;
-		if(flag===1&&index<num){
-			++index;
-		}
-		else if(flag===-1&&index>1){
-			--index;
-		}
-		var go=function(){
-			if(Math.abs(cur-pre)<height){
-				cur=cur+speed*flag;
-				pages.style.marginTop=-1*cur+'px';
-				setTimeout(go,10);
+	//键盘按键事件处理程序
+	function keyEvent(e){
+		var e=e||window.event,
+			keyCode=e.keyCode,
+			flag;
+		if(keyCode===38){
+				flag=-1;
+				scroll(flag);
 			}
-			else{
-				pages.style.marginTop=-1*(pre+height*flag)+'px';
-				cur=pre+height*flag;
-				animation=true;
-			}
+		if(keyCode===40){
+				flag=1;
+				scroll(flag);
 		}
-		go();
 	}
 
+	//页面滚动效果
+	function pageScrollAnimation(from,to){
+		animation=false;
+		var gap=to-from,
+			curP=page[from-1],
+			nextP=page[to-1],
+			nextPStyle=nextP.style;
+		for(var i=0,dir;dir=direction[i];i++){
+			if(dir==='top')
+				nextPStyle[dir]=(gap>0?height:-height)+'px';
+			else{
+				nextPStyle[dir]=(gap>0?width:-width)+'px';
+			}
+		}
+		var nextPClass=handleStyleTable(nextP);
+		var curPClass=handleStyleTable(curP);
+		nextPClass.add('slide');
+		var nextPAnimate=new animate(nextP);
+		nextPAnimate.callback(function(){
+			nextPClass.remove('slide');
+			nextPClass.add('cur');
+			curPClass.remove('cur');
+			animation=true;
+			curNav(num,index);
+			nextPClass=curPClass=null;
+		});	
+		nextPAnimate.start([
+			'opacity:1',
+			'top:0px',
+			'left:0px'
+		],time,'linear');
+	}  
+
+	//设置当前页对应的导航按钮样式
+	function curNav(num,index){
+		var li=ul.getElementsByTagName('li');
+		for(var i=0;i<num;i++){
+			li[i].className='';
+			if(i===index-1){
+				li[i].className='cur';
+			}
+		}
+	}
+	//创建导航栏
+	function createNav(num){
+		ul=document.createElement('ul');
+		ul.id='nav';
+		for(var i=0;i<num;i++){
+			var li=document.createElement('li');
+			li.setAttribute('data-index',i+1);
+			if(i===index-1){
+				li.className='cur';
+			}
+			ul.appendChild(li);
+		}
+		warp.appendChild(ul);
+		addEvent(ul,'click',function(e){
+			var e=e||window.event,
+				target=e.target||e.srcElement,
+				toIndex=target.getAttribute('data-index');
+			if(toIndex){
+				if(animation){
+					if(toIndex-index>0){
+						scrollAnimation(height*(toIndex-index),1);
+					}
+					else if(toIndex-index<0){
+						scrollAnimation(height*(index-toIndex),-1);
+					}
+					index=toIndex;
+				}
+			}
+		});
+	}
+	//定义控制器
+	var controls={
+		mousewheel:function(){
+			if(window.navigator.userAgent.toLowerCase().indexOf('firefox')!=-1){
+				addEvent(doc,'DOMMouseScroll',function(e){
+					wheelEvent(e);
+				});
+			}
+			else{
+				addEvent(doc,'mousewheel',function(e){
+					wheelEvent(e);
+				});
+			}
+		},
+		keyboard:function(){
+			addEvent(doc,'keydown',function(e){
+				keyEvent(e);
+			});
+		}
+
+	}
+	//添加控制器
+	function addControl(control){
+		for(var i=0,len=control.length;i<len;i++){
+			controls[control[i]]();
+		}
+		addEvent(window,'resize',function(){
+			height=getPageSizeInf().docHeight;
+			width=getPageSizeInf().docWidth;
+		});
+	}
+
+	return (function(){
+		if(nav){
+			createNav(num);
+		}
+		init(index);
+		addControl(control);
+	})();
 })(
 	{
-		id:'warp',
-		num:5,
-		direction:'vertical',
-		time:1,
-		style:'normal'
+		id:'warp',//页面的外层包裹
+		num:5,//页面数量
+		direction:['left','top'],//页面跳转的方向 top-垂直，left -水平
+		time:800,//页面跳转的时间，单位为ms
+		style:'normal',//页面跳转的风格 
+		nav:true,//是否开启导航栏
+		loop:true,//是否开启循环页面
+		start:1,//初始页面的位置
+		control:['mousewheel','keyboard']//'mousewheel-鼠标滚轮,keyboard-键盘方向键
 	}
 );
